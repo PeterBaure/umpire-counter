3ee887d5ac5f1cbd19d1a14c72db98a82b0a0881
PeterBaure <100369690+PeterBaure@users.noreply.github.com>
2022-02-24 11:13:26 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.TiltLeft, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.TiltRight, function () {
+	
+})
+basic.showIcon(IconNames.Yes)
+basic.forever(function () {
+	
+})
