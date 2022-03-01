9397189cfd76512f75a8b0afda549bf9e574a2c7
PeterBaure <100369690+PeterBaure@users.noreply.github.com>
2022-03-01 10:27:38 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,32 @@
 input.onButtonPressed(Button.A, function () {
-	
+    Number_of_strikes += 1
+    basic.pause(500)
+    basic.showString("S")
+    basic.showNumber(Number_of_strikes)
+    if (Number_of_strikes == 3) {
+        basic.clearScreen()
+        basic.showString("Out")
+    }
 })
-input.onGesture(Gesture.TiltLeft, function () {
-	
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    number_of_balls = 0
+    Number_of_strikes = 0
 })
 input.onButtonPressed(Button.B, function () {
-	
+    number_of_balls += 1
+    basic.pause(500)
+    basic.showString("B")
+    basic.showNumber(number_of_balls)
+    if (number_of_balls == 4) {
+        basic.clearScreen()
+        basic.showString("Walk")
+    }
 })
-input.onGesture(Gesture.TiltRight, function () {
-	
-})
+let number_of_balls = 0
+let Number_of_strikes = 0
 basic.showIcon(IconNames.Yes)
+basic.clearScreen()
 basic.forever(function () {
 	
 })
